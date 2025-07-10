9cac18e14a0d5e2b4f8e39c0eb7cab702c320675
Sourav Jindal <138982335+souravjindal@users.noreply.github.com>
2025-07-10 14:33:51 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,17 @@
+# Task 1: Basic Mathematical Operations
+
+firstNum=input ("Enter first number:")
+secondNum=input ("Enter second number:")
+firstNum=int(firstNum)
+secondNum=int(secondNum)
+sum=firstNum+secondNum
+print("Addition:",sum)
+
+sub=firstNum-secondNum
+print("Subtraction",sub)
+
+mul=firstNum*secondNum
+print("Multication",mul)
+
+div=firstNum/secondNum
+print("Division",div)
---
+++ Task2.py
@@ -0,0 +1,7 @@
+# Task 2: Personalized Greeting
+
+
+firstName=input("Enter your first name: ")
+lastName=input("Enter your last name: ")
+fullName=(firstName+" "+lastName)
+print("Hello,",fullName ,"!Welcome to the python program")
